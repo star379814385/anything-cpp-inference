298b47ae520324d6444a2cf9091d2af5733866a7
RainfyLee <379814385@qq.com>
2025-03-22 00:18:46 +0800

    chore: 删除多余代码

--- include/onnx/MMYoloONNXInference.h
+++ include/onnx/MMYoloONNXInference.h
@@ -1,14 +1,14 @@
 #include "ONNXInference.h"
 
-class MMYoloONNXInference: public ONNXInference
+class MMYoloONNXInference : public ONNXInference
 {
 public:
     MMYoloONNXInference()
     {
         m_swapRb = true;
         m_downSampleBase = 32;
-        m_keepRatio = false;
-        m_PadAfterResize = false;
+        m_keepRatio = true;
+        m_PadAfterResize = true;
         m_mean[0] = 0.0f;
         m_mean[1] = 0.0f;
         m_mean[2] = 0.0f;
@@ -16,22 +16,19 @@ public:
         m_std[1] = 1.0f * 255;
         m_std[2] = 1.0f * 255;
     };
-    ~MMYoloONNXInference()
-    {
+    ~MMYoloONNXInference() {
 
     };
-protected:    
+
+protected:
     virtual bool update_from_config(const std::string modelDir)
     {
         ONNXInference::update_from_config(modelDir);
-        m_scoreThr = 0;        
+        m_scoreThr = json_data.root["score_thr"].asFloat();
         return true;
     };
 
 protected:
-    // virtual bool postprocess(const std::vector<Ort::Value> &onnx_output, AiData::InnerModelOutput &output, TransformParam transformParam) override;
-    // virtual bool postprocess(const std::vector<Ort::Value> &onnx_output, std::vector<AiData::InnerModelOutput> &outputs, std::vector<TransformParam> transform_param_list) override; 
     virtual bool decode_result(const std::vector<Ort::Value> &onnx_output, std::vector<AiData::InnerModelOutput> &outputs);
     float m_scoreThr;
-
 };
--- include/onnx/ONNXInference.h
+++ include/onnx/ONNXInference.h
@@ -1,3 +1,5 @@
+#ifndef ONNXInference_H
+#define ONNXInference_H
 #include "BaseInference.h"
 #include "onnxruntime_cxx_api.h"
 
@@ -10,27 +12,24 @@ example
 class ONNXInference : public BaseInference
 {
 public:
-    ONNXInference(){};
-    ~ONNXInference(){};
-    // virtual bool inference(const AiData::InnerModelInput &input, AiData::InnerModelOutput &output) override;
+    ONNXInference() {};
+    ~ONNXInference() {};
     virtual bool inference(const AiData::InnerModelInput &input, AiData::InnerModelOutput &output) override;
     virtual bool inference(const std::vector<AiData::InnerModelInput> &inputs, std::vector<AiData::InnerModelOutput> &outputs) override;
+
 protected:
     virtual bool update_from_config(const std::string modelDir)
     {
         BaseInference::update_from_config(modelDir);
         use_cuda = json_data.root["use_cuda"].asBool();
-        num_threads = json_data.root["num_threads"].asInt();      
+        num_threads = json_data.root["num_threads"].asInt();
         return true;
     };
     virtual bool update_model() override;
-    
 
 protected:
     virtual bool preprocess(const cv::Mat &src, cv::Mat &dst, TransformParam &transformParam);
     virtual bool decode_result(const std::vector<Ort::Value> &onnx_output, std::vector<AiData::InnerModelOutput> &outputs) = 0;
-    // virtual bool postprocess(const std::vector<Ort::Value> &onnx_output, AiData::InnerModelOutput &output, TransformParam transformParam) = 0;
-    // virtual bool postprocess(const std::vector<Ort::Value> &onnx_output, std::vector<AiData::InnerModelOutput> &outputs, std::vector<TransformParam> transform_param_list) = 0; 
     virtual bool postprocess(AiData::InnerModelOutput &output, const TransformParam &transformParam);
 
 private:
@@ -42,3 +41,4 @@ private:
     bool use_cuda;
     int num_threads;
 };
+#endif
